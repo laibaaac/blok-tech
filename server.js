713898ca900321b7a6091f49e5abdc6fedf30015
Laiba Choudhry <laiba@hotmail.nl>
2022-03-07 09:48:22 +0100

    id toegevoegd

--- server.js
+++ server.js
@@ -2,9 +2,57 @@ const express = require('express');
 const app = express ();
 const port = 3000;
 
+const restaurants = [
+    {
+      "id":1, 
+      "name": "Namaste",
+    }, 
+    {
+      "id":2, 
+      "name": "Queen R",
+    }, 
+    {
+      "id":3, 
+      "name": "Ramen ya",
+    }, 
+    {
+      "id":4, 
+      "name": "Poke",
+    }, 
+    {
+      "id":5, 
+      "name": "Istanbul",
+    }, 
+    {
+      "id":6, 
+      "name": "Saigon",
+    }, 
+    {
+      "id":7, 
+      "name": "Pho 91",
+    }, 
+    {
+      "id":8, 
+      "name": "Thai",
+    },  {
+      "id":9, 
+      "name": "Grill",
+    },  {
+      "id":10, 
+      "name": "Pizzeria",
+    }, 
+  ;
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('<h1>Restaurant</h1>')
   })
+
+const restaurants = [
+];
   
+app.get('/test', (req, res) => {
+    res.render
+} )
+
 //listen on port 3000
 app.listen(port, () => console.info (`listening on port ${port}`))
