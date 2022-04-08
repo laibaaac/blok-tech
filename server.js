885c63776821c21308787256a19f95f90410abde
Laiba Choudhry <laiba@hotmail.nl>
2022-04-08 19:40:50 +0200

    errorrrrr

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const res = require('express/lib/response');
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv').config();
-const { MongoClient }=require('mongodb');
-const { ObjectID } = require('mongodb');
+const {
+  MongoClient
+} = require('mongodb');
+const {
+  ObjectID
+} = require('mongodb');
 const bodyParser = require('body-parser');
 const slug = require('slug');
 
@@ -13,9 +17,11 @@ const slug = require('slug');
 /********
   middleware
  */
-app.use(express.static('views'))
+app.use(express.static('public'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 let db = null;
 
@@ -26,7 +32,7 @@ app.set('view engine', 'ejs');
 
 
 
-// Startpunt, de gebruiker zijn/haar locatie
+/*// Startpunt, de gebruiker zijn/haar locatie
 const start = {
   latitude: 52.370216,
   longitude: 4.895168
@@ -57,39 +63,43 @@ console.log(distance.toFixed(2))
 
 // restaurants.sort(compare)
 
+*/
 
-    app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
       res.send('<h1>Restaurant</h1>')
     })
+*/
+app.get('/about', (req, res) => {
+  res.render("about")
+});
 
-    app.get('/about', (req, res) => {
-      res.render("about")
-    })
-    
-    //app.get ('/restaurant/:id', (req, res) =>{
-      //const restaurant = restaurant.find(element =>element.id == id)
-    //  const id = req.params.id
-  
+//app.get ('/restaurant/:id', (req, res) =>{
+//const restaurant = restaurant.find(element =>element.id == id)
+//  const id = req.params.id
 
 
-    /*****************************************************
-    * Connect to database
-    ****************************************************/
-    /*async function connectDB() {​
-    consturi = process.env.DB_URI;
-    constclient = newMongoClient(uri, {​
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    }​);
-    try {​
-    awaitclient.connect();
+
+/*****************************************************
+ * Connect to database
+ ****************************************************/
+async function connectDB() {
+
+  const uri = process.env.DB_URI;
+  const client = newMongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  try {
+    await client.connect();
     db = client.db(process.env.DB_NAME);
-    }​ catch (error) {​
-    throwerror;
-    }​
-    }​
-  */
-  app.get('/', async (req, res) => {
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+
+/*app.get('/', async (req, res) => {
       // GET LIST OF MOVIES
       const query = {"location": "amsterdam"}
      // const options = {sort : {location:amsterdam}}
@@ -113,7 +123,7 @@ console.log(distance.toFixed(2))
     res.render('moviedetails', {title, movie});
 });
 
-  app.post('/restaurant/add', async (req, res) => {
+  /*app.post('/restaurant/add', async (req, res) => {
     // ADD 
     let restaurant = {
         slug: slug(req.body.name),
@@ -125,12 +135,12 @@ console.log(distance.toFixed(2))
     const options = {sort : {location:amsterdam}}
     //hier word er soort van gefilterd
     const restaurant = await db.collection('restaurant').find(query, options).toArray();
-  })
+*/
 
 
 
 
-    app.post ('/ditistijdelijk', (req, res) => {
+/* app.post ('/ditistijdelijk', (req, res) => {
       console.log(req.body);
       const restaurant = {
         name: req.body.eenvariabele
@@ -141,24 +151,25 @@ console.log(distance.toFixed(2))
       title = "hetisgelukt"
       res.render('restaurant', ('hetisgelukt', {restaurant}));
     })
+*/
+
+app.use((req, res, next) => {
+  res.status(404).send('Error 404: file not found')
+});
+//dit gebeurdt er wanneer ik een pagina heb die niet in mijn route ontstaat
 
 
-    app.use ((req, res, next) => {
-      res.status(404).send('Error 404: file not found')
-    });
-    //dit gebeurdt er wanneer ik een pagina heb die niet in mijn route ontstaat
+/*  app.get('/restaurant/add', (req, res) =>{
+    const title = "Add a new restaurant";
+    res.render('addrest', {title});
+  });
 
-
-    app.get('/restaurant/add', (req, res) =>{
-      const title = "Add a new restaurant";
-      res.render('addrest', {title});
-    });
-
-    async function connectionDB() {
-      const uri = process.env.DB_URI;
-    }
-    /*** Start webserver */
-    app.listen(port, ()=> {
-      connectionDB().then => console.log("we have a connection to mongo!");
-      console.info(`listening on port ${port}`)
-    });
+ /* async function connectionDB() {
+    const uri = process.env.DB_URI;
+  }
+  
+  /*** Start webserver */
+app.listen(port, () => {
+  //  connectionDB().then( () => console.log("we have a connection to mongo!")) ;
+  console.log(`Example app listening on port ${port}`)
+});
