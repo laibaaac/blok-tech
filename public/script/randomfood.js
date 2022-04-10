51a18a4decaa73b19fb21df675093b871c020b44
Laiba Choudhry <laiba@hotmail.nl>
2022-04-10 10:13:12 +0200

    api toegevoegd maar werkt niet echt

---
+++ public/script/randomfood.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+let newRandomFood = document.querySelector(".generateFood");
+
+console.log("ik heb een api");
+
+async function getRandomFood() {
+  const res = await fetch( "https://foodish-api.herokuapp.com/");
+  const eten = await res.json();
+  showFood(eten);
+}
+
+//insert random food in text field
+function showFood(eten) {
+  let foodThumbnail = document.querySelector(".etenThumbnail");
+  let foodTextField = document.querySelector(".randomFood");
+
+  let foodImage = eten.drinks[0].strDrinkThumb;
+  foodThumbnail.src = foodImage;
+
+  let foodName = eten.drinks[0].strDrink;
+  foodTextField.innerHTML = foodName;
+}
--- server.js
+++ server.js
@@ -1,178 +1,103 @@
-const haversine = require('haversine');
-const express = require('express');
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-useless-catch */
+/* eslint-disable no-undef */
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line quotes
 const res = require('express/lib/response');
 const app = express();
+const router = express.Router();
 const port = 3000;
-const dotenv = require('dotenv').config();
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-unused-vars
+const dotenv = require("dotenv").config();
 const {
   MongoClient
-} = require('mongodb');
+} = require("mongodb");
 const {
+  // eslint-disable-next-line no-unused-vars
   ObjectID
-} = require('mongodb');
-const bodyParser = require('body-parser');
+} = require("mongodb");
+const bodyParser = require("body-parser");
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line quotes
 const slug = require('slug');
 
 
 /********
   middleware
  */
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}))
-
+}));
 let db = null;
 
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
+const filter = { location: " " };
+//routes
 
-
-
-
-/*// Startpunt, de gebruiker zijn/haar locatie
-const start = {
-  latitude: 52.370216,
-  longitude: 4.895168
-}
-
-// Locatie van het restaurant
-const end = {
-  latitude: 52.370323,
-  longitude: 5.215608
-}
-
-// Bereken de afstand in meters tussen het start en eindpunt, gebruiker en restaurant. Dit moet je per restaurant doen
-let distance = haversine(start, end, {unit: 'meter'}) / 1000;
-
-// Hiermee rond je het getal af op 2 decimalen
-console.log(distance.toFixed(2))
-
-// function compare(a, b) {
-//   if ( a.distance < b.distance ){
-//     return -1;
-//   }
-//   if ( a.distance > b.distance ){
-//     return 1;
-//   }
-//   return 0;
-// }
-
-
-// restaurants.sort(compare)
-
-*/
-
-/* app.get('/', (req, res) => {
-      res.send('<h1>Restaurant</h1>')
-    })
-*/
-
-app.get('/about', (req, res) => {
+// eslint-disable-next-line quotes
+app.post(`/ `, async (req, res) => {
+  console.log(req.body);
   res.render("about");
+  
 });
 
-app.get ('/restaurant/:id', (req, res) =>{
-const restaurant = restaurant.find(element =>element.id == id);
-const id = req.params.id;
+app.get('/result', async (req, res) => {
+  res.render('result');
 });
 
+// const result = await gebruiker.updateOne(filter, updateDoc, options);
 
+app.get('/favoriet', async (req, res) => {
+  res.render("favoriet");
+
+});
+
+app.post('/favoriet/delete', async (req, res) => {
+
+});
+
+app.get("/food", (req, res) => {
+  res.render("food", { title: "food"});
+});
 
 /*****************************************************
  * Connect to database
  ****************************************************/
+ // eslint-disable-next-line no-unused-vars
  async function connectDB() {
-  const uri = process.env.DB_URI
+  const uri = process.env.DB_URI;
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
   try {
-    await client.connect()
-    db = client.db(process.env.DB_NAME)
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 
-
-/*app.get('/', async (req, res) => {
-      // GET LIST OF MOVIES
-      const query = {"location": "amsterdam"}
-     // const options = {sort : {location:amsterdam}}
-      //hier word er soort van gefilterd
-      const restaurant = await db.collection('restaurant').find(query, options).toArray();
-  
-      // RENDER PAGE
-     // const title  = (movies.length == 0) ? "No movies were found" : "Movies";
-     // res.render('', {title, movies});
-  });
-    
-  app.get('/movies/:movieId', async (req, res) => {
-
-    // FIND restaurant
-    const id = req.params.restaurantId;
-    const query = {_id: ObjectId(req.params.restaurantId)}
-    const restaurant = await db.collection('restaurant').findOne(query);
-
-    // RENDER PAGE
-    const title = `Moviedetails for ${movie.name}`;
-    res.render('moviedetails', {title, movie});
-});
-*/
-
-  /*app.post('/restaurant/add', async (req, res) => {
-    // ADD 
-    let restaurant = {
-        slug: slug(req.body.name),
-        name: req.body.name, 
-       location: req.body.location
-    };
-    await db.collection('restaurant').insertOne(movie);
-    const query = {"location": "amsterdam"}
-    const options = {sort : {location:amsterdam}}
-    
-    //hier word er soort van gefilterd
-    const restaurant = await db.collection('restaurant').find(query, options).toArray();
-*/
-
-
-
-
-/* app.post ('/ditistijdelijk', (req, res) => {
-      console.log(req.body);
-      const restaurant = {
-        name: req.body.eenvariabele
-
-      }
-      restaurant.push(restaurant)
-    
-      title = "hetisgelukt"
-      res.render('restaurant', ('hetisgelukt', {restaurant}));
-    })
-*/
-
+// eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => {
-  res.status(404).send('Error 404: file not found')
+  res.status(404).send("Error 404: file not found");
 });
 //dit gebeurdt er wanneer ik een pagina heb die niet in mijn route ontstaat
 
-
-/*  app.get('/restaurant/add', (req, res) =>{
-    const title = "Add a new restaurant";
-    res.render('addrest', {title});
-  });
-
- /* async function connectionDB() {
+ async function connectionDB() {
     const uri = process.env.DB_URI;
   }
   
   /*** Start webserver */
 app.listen(port, () => {
-  //  connectionDB().then( () => console.log("we have a connection to mongo!")) ;
-  console.log(`Example app listening on port ${port}`)
+ connectionDB().then( () => console.log("we have a connection to mongo!")) ;
+  console.log(`Example app listening on port ${port}`);
 });
